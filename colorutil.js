5ba45f7d92127900be997e70a6645c3abf471150
Student <student@RU-019.local>
2015-10-05 15:10:48 -0600

    json added

--- colorutil.js
+++ colorutil.js
@@ -7,6 +7,6 @@ var b = process.argv[4]
 
 module.exports = {
 luminosity: 0.2126*r + 0.7152*g + 0.0722*b,
-darken: (0.8*0.2126*r + 0.8*0.7152*g + 0.8*0.0722*b)
+darken: [(0.8*r) , (0.8*g),  (0.8*b)]
 };
 
---
+++ project/webcolors.js
@@ -0,0 +1,4 @@
+var request = require('request')
+
+request
+.get(webcolors.json)
